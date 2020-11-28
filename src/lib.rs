71bc01fcc47e3ba7cc7afca5ba8e202649e0c1f6
Mendelt Siebenga <msiebenga@gmail.com>
2020-11-29 00:41:10 +0100

    log requests

--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,23 @@ use tide::{Middleware, Next, Request, Result};
 pub struct LogMiddleware {}
 
 impl LogMiddleware {
-    fn log_request(&self) {}
+    fn log_request<State>(&self, request: &Request<State>) {
+        log::info!(
+            "<-- Request received, method: {}, path {}",
+            request.method(),
+            request.url().path()
+        );
+    }
 
     fn log_response(&self) {}
 }
 
 #[async_trait::async_trait]
 impl<State: Clone + Send + Sync + 'static> Middleware<State> for LogMiddleware {
-    async fn handle(&self, req: Request<State>, next: Next<'_, State>) -> crate::Result {
-        self.log_request();
+    async fn handle(&self, request: Request<State>, next: Next<'_, State>) -> crate::Result {
+        self.log_request(&request);
 
-        let result = next.run(req).await;
+        let result = next.run(request).await;
 
         self.log_response();
         Ok(result)
