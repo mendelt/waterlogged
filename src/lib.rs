263c18863eecbf54475f945785e1322ca804f54f
Mendelt Siebenga <msiebenga@gmail.com>
2020-11-28 23:58:56 +0100

    set up middleware

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use tide::{Middleware, Next, Request, Result};
 
 pub struct LogMiddleware { }
 
@@ -9,5 +10,12 @@ impl LogMiddleware {
 
     fn log_response() {
 
+    }
+}
+
+#[async_trait::async_trait]
+impl<State: Clone + Send + Sync + 'static> Middleware<State> for LogMiddleware {
+    async fn handle(&self, req: Request<State>, next: Next<'_, State>) -> crate::Result {
+        Ok(next.run(req).await)
     }
 }
