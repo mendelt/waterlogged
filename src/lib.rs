204ca8ab285f8e65dca4d5e3999739f087a74a7a
Mendelt Siebenga <msiebenga@gmail.com>
2020-11-28 23:54:09 +0100

    set up LogMiddleware struct and impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+
+pub struct LogMiddleware { }
+
+impl LogMiddleware {
+
+    fn log_request() {
+
+    }
+
+    fn log_response() {
+
     }
 }
